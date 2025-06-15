1a37902d82c2460c8bb4dcc90bbbd4f148de6fed
Taco Miguel <miguel.taco@unmsm.edu.pe>
2025-06-15 00:35:05 -0500

    commit1

---
+++ plataforma-web/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px' }}>
+      <h1>Tutor Virtual</h1>
+      <p>Bienvenido al sistema de ayuda en matemáticas</p>
+    </div>
+  );
+}
+
+export default App;
